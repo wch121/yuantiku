f53c55a66da044aaf0176920bb012ed60a1d9980
wch <1165553576@qq.com>
2022-12-08 23:53:56 +0800

    引入ui框架

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Button from '@material-ui/core/Button';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Button variant="contained" color="primary">你好，世界</Button>
+            </div>
+
+        )
+    }
+}
